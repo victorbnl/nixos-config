c577a53228527c432be396428f57e88d978d072b
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-09-29 22:48:51 +0200

    Add vim package

--- configuration.nix
+++ configuration.nix
@@ -74,10 +74,10 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #   wget
-  # ];
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
