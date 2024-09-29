a9d814b8997fbe1231b45e3fe36dd831cba9833b
Victor Bonnelle <victor.bonnelle@protonmail.com>
2024-09-29 23:28:52 +0200

    Add victor user

--- configuration.nix
+++ configuration.nix
@@ -63,14 +63,14 @@
   # services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  users.users.victor = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   #   packages = with pkgs; [
   #     firefox
   #     tree
   #   ];
-  # };
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
